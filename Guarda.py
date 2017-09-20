7604b20e078be9266a60448b59b5b420dd9249d3
Allan Gonçalves <allangoncalves@outlook.com>
2017-09-20 18:03:09 -0300

    Add files via upload

--- Guarda.py
+++ Guarda.py
@@ -5,60 +5,80 @@ import hmac, os, json, sys
 class Guarda():
 
 	def __init__(self):
-		self.files = {}
-		self.early = {}
+		self.files = {}#Dicionario com os arquivos atuais
+		self.early = {}#Dicionario com os arquivos antigos
+		self.changed = {}#alterações realizadas
 
 	def loadData(self):
 		for directory, folderName, file in os.walk("."):
 				for name in file:
-					if name != ".table":
+					if name != ".hashs" and name != ".changes":
 						fullName = os.path.join(directory, name).decode('unicode-escape')
 						self.files[fullName] = None
 		
-		if os.path.isfile(".table"):
-			with open(".table", "r") as input:
+		if os.path.isfile(".hashs"):
+			with open(".hashs", "r") as input:
 				file_str  = input.read()
 				self.early = json.loads(file_str)
 
+		if os.path.isfile(".changes"):
+			with open(".changes", "r") as input:
+				file_str  = input.read()
+				self.changed = json.loads(file_str)
 		self.calculateHash()
 
-		diff = set(self.files.items()) ^ set(self.early.items())
-		for d in diff:
-			print("{} foi removido".format(d))
-
 	def saveData(self):
-		with open(".table", "w") as inputFile:
-			jsondata = json.dumps(g.files, ensure_ascii=True, indent=4)
-			inputFile.write(jsondata)
+			with open(".changes", "w") as inputFile:
+				jsondata = json.dumps(self.changed, ensure_ascii=True, indent=4)
+				inputFile.write(jsondata)
+
+			with open(".hashs", "w") as inputFile:
+				jsondata = json.dumps(self.files, ensure_ascii=True, indent=4)
+				inputFile.write(jsondata)
 
 	def calculateHash(self):
 			for fullName in self.files:
-				print("Opening file:{}".format(fullName))
 				with open(fullName, "r") as input:
 					file_str  = input.read()
 					myhash = hmac.new(file_str).hexdigest().decode('unicode-escape')
 					if fullName not in self.early:
-						self.files[fullName] = myhash
-						self.early[fullName] = myhash
-						print("\t@@@ {} foi adicionado @@@".format(fullName))
+						self.changed[fullName] = 0
 					elif not self.early[fullName] == myhash:
-						print("\t@@@ {} foi alterado @@@".format(fullName))
-						ans = raw_input("\t### Deseja salvar as alterações?(S/N) ###\n")
-						if(ans.lower() == "s"):
-							self.files[fullName] = myhash
-							self.early[fullName] = myhash
-						else:
-							self.files[fullName] = self.early[fullName]
-					else:
-						self.files[fullName] = myhash
-						self.early[fullName] = myhash
+						self.changed[fullName] = 1
+					self.files[fullName] = myhash
+					self.early[fullName] = myhash
+
+	def verifyChanges(self):
+		diff = set(self.files.items()) ^ set(self.early.items())##DIFERENÇA SIMETRICA ENTRE OS ARQUIVOS QUE TINHAM ANTES E DEPOIS
+
+		for item in self.changed:
+			if int(self.changed[item]) == 0:
+				print("@@@ {} foi adicionado. @@@".format(item))
+			elif int(self.changed[item]) == 1:
+				print("@@@ {} foi alterado. @@@".format(item))
+			self.changed[item] = 2
+		for d in diff:
+			print("@@@ {} foi removido. @@@".format(d))
+
 
 if __name__ == '__main__':
 
-	if len(sys.argv) >= 2:
-		os.chdir(sys.argv[1])
+	if len(sys.argv) >= 3:
 		g = Guarda()
-		g.loadData()
-		g.saveData()
+		if sys.argv[1]=="-i" or sys.argv[1] == "-t":
+			os.chdir(sys.argv[2])
+			g.loadData()
+			if sys.argv[1] == "-t":
+				g.verifyChanges()
+			g.saveData()
+		elif sys.argv[1]=="-x":
+			os.chdir(sys.argv[2])
+			if os.path.isfile(".hashs") and os.path.isfile(".changes"):
+				os.remove(".hashs")
+				os.remove(".changes")
+			else:
+				print("Você não criou ou já removeu o arquivo de Hashs")
+		else:
+			print("OPÇÃO INVALIDA!")
 	else:
-		print("Insira o diretório a ser guardado")
+		print("Argumentos insuficientes.")
